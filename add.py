eacb67939e8c519929b0b05dae8bb503524b3f11
john2829 <johnpeter1329@gmail.com>
2022-02-11 16:01:30 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,24 @@
+add1 = input("Enter first digit of addition:")
+add2= input("Enter second digit of addition:")
+total = float(add1) + float(add2)
+print("addition of two no is :",(total))
+
+sub1 = input("Enter first digit of subtraction :")
+sub2 = input("Enter second digit of subtraction :")
+total = float(sub1) - float(sub2)
+print("subtraction of two no is :",(total))
+
+mul1 = input("Enter first digit of multiplication :")
+mul2 = input("Enter second digit of multiplication:")
+total = float(mul1) * float(mul2)
+print("multiplication of two no is :",(total))
+
+div1 = input("Enter first digit of division :")
+div2 = input("Enter second digit of division :")
+total = float(div1) / float(div2)
+print("Division of two no is :",(total))
+
+mod1 = input("Enter first digit of modulus:")
+mod2 = input("Enter second digit of modulus:")
+total = float(mod1) % float(mod2)
+print("modulus of two no is :",(total))
